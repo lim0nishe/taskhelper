738ce9c54201109321d76842da79de08579b72d2
Andrey Adamovich <lem000n@mail.ru>
2017-08-13 23:14:45 +0700

    added /user request

--- src/main/java/com/itolla/test/taskhelper/controller/UserController.java
+++ src/main/java/com/itolla/test/taskhelper/controller/UserController.java
@@ -8,22 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
     @RequestMapping(method = RequestMethod.GET)
-    public List<User> getUsers(){
-        return userRepository.findAll();
-    }
-
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public User getUser(@PathVariable("id") Long id){
-        return userRepository.findOne(id);
+    public User getUserInfo(Principal principal){
+        return userRepository.findOneByUsername(principal.getName());
     }
 }
