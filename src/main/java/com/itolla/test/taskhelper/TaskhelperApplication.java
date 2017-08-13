545879f821886c298de1714b877893d4830ac61c
Andrey Adamovich <lem000n@mail.ru>
2017-08-13 23:06:17 +0700

    added authorization

--- src/main/java/com/itolla/test/taskhelper/TaskhelperApplication.java
+++ src/main/java/com/itolla/test/taskhelper/TaskhelperApplication.java
@@ -4,9 +4,11 @@ package com.itolla.test.taskhelper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 
 @SpringBootApplication
+@EnableResourceServer
 public class TaskhelperApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/itolla/test/taskhelper/model/User.java
+++ src/main/java/com/itolla/test/taskhelper/model/User.java
@@ -2,14 +2,17 @@ package com.itolla.test.taskhelper.model;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 @Table(schema = "public")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -51,9 +54,35 @@ public class User {
         return this.username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
     public void setPassword(String password){
         this.password = password;
     }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
     public String getPassword(){
         return this.password;
     }
